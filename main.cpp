ab8ec7ac62fc0f5a353607f3c897cbeef67bbedd
cvrain <cvraindays@outlook.com>
2026-02-07 18:33:59 +0800

    初始化项目

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreationFailed,
+        &app,
+        []() { QCoreApplication::exit(-1); },
+        Qt::QueuedConnection);
+    engine.loadFromModule("rainer_blog_dashboard", "Main");
+
+    return app.exec();
+}
